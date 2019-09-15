5ebe1f51601fca8a0abad6776c629b0811e97ab5
budougumi0617 <budougumi0617@gmail.com>
2019-09-15 21:32:39 +0900

    :heart: use HTTPS

--- dkl.rb
+++ dkl.rb
@@ -6,11 +6,11 @@ class Dkl < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "http://github.com/budougumi0617/dkl/releases/download/v0.0.0/dkl_0.0.0_Darwin_x86_64.tar.gz"
+    url "https://github.com/budougumi0617/dkl/releases/download/v0.0.0/dkl_0.0.0_Darwin_x86_64.tar.gz"
     sha256 "d567ace51cee949e87ed7be41c0126fe407c5b7f3c5e91e3288981840a3e889d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "http://github.com/budougumi0617/dkl/releases/download/v0.0.0/dkl_0.0.0_Linux_x86_64.tar.gz"
+      url "https://github.com/budougumi0617/dkl/releases/download/v0.0.0/dkl_0.0.0_Linux_x86_64.tar.gz"
       sha256 "cb9117a8e1bdc93f4aa5859a77e8ce1324a3dd888a4f2822308296edd869573c"
     end
   end
