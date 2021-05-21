98950e63796cec3caa870d8f748530ae93a4c9bc
budougumi0617 <budougumi0617@gmail.com>
2021-05-21 11:20:25 +0900

    Brew formula update for gigi version v0.0.12

--- gigi.rb
+++ gigi.rb
@@ -5,24 +5,24 @@
 class Gigi < Formula
   desc "Check pull request size."
   homepage "https://github.com/budougumi0617/gigi"
-  version "0.0.11"
+  version "0.0.12"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/budougumi0617/gigi/releases/download/v0.0.11/gigi_0.0.11_Darwin_x86_64.tar.gz"
-    sha256 "e2a21875d87b7a3e6c745fed3979f8874cf60145c275755c6d3980b9ce0fae70"
+    url "https://github.com/budougumi0617/gigi/releases/download/v0.0.12/gigi_0.0.12_Darwin_x86_64.tar.gz"
+    sha256 "359e14be28fead24096e996aeafe8c46cc85e781285dca58be235979d327b06b"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/budougumi0617/gigi/releases/download/v0.0.11/gigi_0.0.11_Darwin_arm64.tar.gz"
-    sha256 "95c02449f51533b69042a01f58f4902d334ef3bc9e9d98f00a05cd016bf4ef38"
+    url "https://github.com/budougumi0617/gigi/releases/download/v0.0.12/gigi_0.0.12_Darwin_arm64.tar.gz"
+    sha256 "b229fef9eb9add5e74b4742319339dd041ac351fd47e50f64f990c13906b7d58"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/budougumi0617/gigi/releases/download/v0.0.11/gigi_0.0.11_Linux_x86_64.tar.gz"
-    sha256 "5b253ec4b898d22659daab68ee59007de3dc25993554525ea45339d75d77637e"
+    url "https://github.com/budougumi0617/gigi/releases/download/v0.0.12/gigi_0.0.12_Linux_x86_64.tar.gz"
+    sha256 "9382791e8e33e5e5ca20cf03735654d26d4bcf6c4119ef379b91bafff1c443a5"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/budougumi0617/gigi/releases/download/v0.0.11/gigi_0.0.11_Linux_arm64.tar.gz"
-    sha256 "a426d7e182b4a6ebf6296035be22bef66bbe04e10c3199dbc8090aa2a563c27d"
+    url "https://github.com/budougumi0617/gigi/releases/download/v0.0.12/gigi_0.0.12_Linux_arm64.tar.gz"
+    sha256 "b3dd8811918f17af88a5248bc2c4ea9c049e8999333419f76da74babb9b9d846"
   end
 
   def install
